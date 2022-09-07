d31384cd4dcc1d78892f0e50e9c8b11347400dd9
Dowan Kim <purplecofe2@gmail.com>
2022-09-07 16:23:22 +0900

    Fix checking matching condition.

--- content.js
+++ content.js
@@ -18,7 +18,7 @@ function doPatchURL() {
     var iptext = String(ipchecktext).replace(/\./gi,"\\\.");
     var ipregex = new RegExp("http\:\/\/"+iptext+"\/tm\/[^\<]+","gi");
     var ipgetregex = new RegExp("http\:\/\/"+"(\\d+\\.\\d+\\.\\d+\\.\\d+)"+"\/tm\/[^\<]+","gi");
-	var ipnewtext = "";
+    var ipnewregr = null;
 	var found = 0;
 
     var elements = document.getElementsByTagName('script');
@@ -29,40 +29,39 @@ function doPatchURL() {
             var node = element.childNodes[j];
 
             if (node.nodeType === 3) {
-                var text = node.nodeValue;
-                var replacedText = text.replace(ipregex, document.URL+"?\"");
+                var text = node.nodeValue;                
+                
+                // detect IP                
+                ipnewregr = ipgetregex.exec(text);
+                if (ipchecktext == "0.0.0.0" && ipnewregr != null) {
+                    ipchecktext = ipnewregr[1];
+                    chrome.storage.sync.set({'AntiKTIP':ipchecktext},function(){
+                        // stored
+                    });
+                    console.log(ipchecktext);
+                }
 
-                if (ipchecktext != "0.0.0.0" && replacedText !== text) {
+                if (ipchecktext != "0.0.0.0" && ipnewregr != null) {
+                    var replacedText = text.replace(ipregex, document.URL+"?\"");
                     element.replaceChild(document.createTextNode(replacedText), node);
                     browser.runtime.sendMessage({redirect: document.URL+"?"});
                     console.log(element);
-                } else {
-					// detect IP
-					if (ipchecktext == "0.0.0.0") {
-						ipnewtext = node.nodeValue;
-						ipnewregr = ipgetregex.exec(ipnewtext);
-						if (ipnewregr != null) {
-							ipchecktext = ipnewregr[1];
-							chrome.storage.sync.set({'AntiKTIP':ipchecktext},function(){
-								// stored
-							});
-							console.log(ipchecktext);
-						}
-					}
-				}
+                }
             }
         }
     }
 
     var elements = document.getElementsByTagName('iframe');
+    ipnewregr = null;
 
     for (var i = 0; i < elements.length; i++) {
         var element = elements[i];
         var text=element.getAttribute("src");
         if(text!=null) {
-            var replacedText = text.replace(ipregex, document.URL+"?");
+            ipnewregr = ipgetregex.exec(text);
             
-            if (ipchecktext != "0.0.0.0" && replacedText !== text) {
+            if (ipchecktext != "0.0.0.0" && ipnewregr != null) {
+              var replacedText = text.replace(ipregex, document.URL+"?");
               element.setAttribute("src",replacedText);
               browser.runtime.sendMessage({redirect: document.URL+"?"});
               console.log(element);
--- redirect.js
+++ redirect.js
@@ -1,5 +1,6 @@
 function handleMessage(request, sender, sendResponse) {
     browser.tabs.update(sender.tab.id, {url: request.redirect});
+    console.log(request.redirect);
 }
 
 browser.runtime.onMessage.addListener(handleMessage);
